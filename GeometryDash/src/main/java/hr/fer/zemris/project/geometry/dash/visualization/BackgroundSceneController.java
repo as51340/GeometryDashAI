6e654db1bbcae1d72a5e1aaf680b58a29100a49b
Matija Frandolić <matija.frandolic@fer.hr>
2020-10-24 00:24:22 +0200

    Add dynamic background in game menu

--- GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/visualization/BackgroundSceneController.java
+++ GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/visualization/BackgroundSceneController.java
@@ -1,15 +1,78 @@
 package hr.fer.zemris.project.geometry.dash.visualization;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import hr.fer.zemris.project.geometry.dash.model.settings.GameConstants;
-import hr.fer.zemris.project.geometry.dash.view.components.GeometryDashTitle;
-import javafx.event.ActionEvent;
+import javafx.animation.Animation;
+import javafx.animation.FillTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.util.Duration;
 
 public class BackgroundSceneController {
+
+	private static final int BACKGROUND_WIDTH = 800;
+	private static final int BACKGROUND_TRANSITION_DURATION = 30000;
+	private static final int COLOR_TRANSITION_DURATION = 7000;
+
+	@FXML
+	private ImageView background1;
+
+	@FXML
+	private ImageView background2;
+
+	@FXML
+	private ImageView background3;
+	
+	@FXML
+	private Rectangle rectangle;
+
+	@FXML
+	public void initialize() {
+		ParallelTransition backgroundCyclingAnimation = new ParallelTransition(
+			createBackgroundTransition(background1), 
+			createBackgroundTransition(background2), 
+			createBackgroundTransition(background3)
+		);
+		backgroundCyclingAnimation.setCycleCount(Animation.INDEFINITE);
+		backgroundCyclingAnimation.play();
 		
-    
+		SequentialTransition colorCyclingAnimation = new SequentialTransition(
+			createColorTransition(rectangle, Color.DODGERBLUE, Color.BLUEVIOLET),
+			createColorTransition(rectangle, Color.BLUEVIOLET, Color.PURPLE),
+			createColorTransition(rectangle, Color.PURPLE, Color.CRIMSON),
+			createColorTransition(rectangle, Color.CRIMSON, Color.ORANGE), 
+			createColorTransition(rectangle, Color.ORANGE, Color.YELLOWGREEN), 
+			createColorTransition(rectangle, Color.YELLOWGREEN, Color.LIGHTGREEN),
+			createColorTransition(rectangle, Color.LIGHTGREEN, Color.CYAN),
+			createColorTransition(rectangle, Color.CYAN, Color.DODGERBLUE)
+		);
+		colorCyclingAnimation.setCycleCount(Animation.INDEFINITE);
+		colorCyclingAnimation.play();
+	}
+	
+	private static TranslateTransition createBackgroundTransition(Node background) {
+		TranslateTransition transition = new TranslateTransition(
+			Duration.millis(BACKGROUND_TRANSITION_DURATION), background
+		);
+		transition.setFromX(0);
+		transition.setToX(-BACKGROUND_WIDTH);
+		transition.setInterpolator(Interpolator.LINEAR);
+		return transition;
+	}
+	
+	private static FillTransition createColorTransition(Shape shape, Color fromColor, Color toColor) {
+		return new FillTransition(
+			Duration.millis(COLOR_TRANSITION_DURATION), 
+			shape, 
+			fromColor, 
+			toColor
+		);
+	}
+
 }
