9785f57beee1446f31eadec928df95293f5e4586
Matija Frandolić <matija.frandolic@fer.hr>
2020-10-24 17:49:27 +0200

    Add resize support

--- GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/GeometryDash.java
+++ GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/GeometryDash.java
@@ -3,57 +3,45 @@ import java.io.IOException;
 
 import hr.fer.zemris.project.geometry.dash.model.GameEngine;
 import hr.fer.zemris.project.geometry.dash.model.settings.GameConstants;
-import hr.fer.zemris.project.geometry.dash.model.settings.music.BackgroundMusicPlayer;
-import hr.fer.zemris.project.geometry.dash.view.components.GeometryDashTitle;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GeometryDash extends Application {
 	
-	
 	private GameEngine gameEngine = new GameEngine(60, "Geometry Dash", GameConstants.WIDTH, GameConstants.HEIGHT);
-	
-	//ovo ti netreba
-	private Pane root = new Pane();
-	
-	
-	
-	/**
-	 * adds title to root scene
-	 */
-	//OVO MAKNI I PODESI U fxmlu
-	private void addTitle() {
-//		GeometryDashTitle title = new GeometryDashTitle("GEOMETRY DASH");
-//		
-//		title.setTranslateX(WIDTH / 2 - title.getTitleWidth() / 2);
-//	    title.setTranslateY(HEIGHT / 3);
-//	    
-//		root.getChildren().add(title);
-		
-	}
-	
-	/**
-	 * creates context for root scene
-	 * @return
-//	 */
-//	private Parent createContext() {
-//		addTitle();
-//		return root;
-//	}	
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-    	//OVO OSTAVI
-    	Parent root1 = FXMLLoader.load(getClass().getResource(GameConstants.pathToVisualization + "BackgroundScene.fxml"));
-    	Scene scene = new Scene(root1);
-		//
+    	Parent root = FXMLLoader.load(
+    		getClass().getResource(GameConstants.pathToVisualization + "BackgroundScene.fxml")
+    	);
     	
+        // Set a default "standard" or "100%" resolution
+        double origW = GameConstants.WIDTH;
+        double origH = GameConstants.HEIGHT;
+        
+        // Place the Group in a StackPane, which will keep it centered
+        StackPane rootPane = new StackPane();
+        rootPane.getChildren().add(root);
+        
+        // Create the scene initially at the "100%" size
+        Scene scene = new Scene(rootPane, origW, origH);
+        
+        // Bind the scene's width and height to the scaling parameters on the group
+        root.scaleXProperty().bind(scene.widthProperty().divide(origW));
+        root.scaleYProperty().bind(scene.heightProperty().divide(origH));
+        
+        // Preserve aspect ratio when resizing
+        primaryStage.minHeightProperty().bind(primaryStage.widthProperty().multiply(0.5));
+        primaryStage.maxHeightProperty().bind(primaryStage.widthProperty().multiply(0.5));
+
         gameEngine.createStageFromData(primaryStage);
-        gameEngine.start();	
+        gameEngine.start();
+        
 //    	tester
 //		Media media = new Media(getClass().getResource(GameConstants.pathToSongs + "BlahBlahBlah.mp3").toExternalForm());
 //		MediaPlayer mediaPlayer = new MediaPlayer(media);
@@ -61,12 +49,10 @@ public class GeometryDash extends Application {
 //		mediaPlayer.play();
 //        Utils.createMediaPlayer("BlahBlahBlah.mp3").play();
 //        System.out.println("Printaj odmah poslije");
-		primaryStage.setTitle("Geometry dash");
-		primaryStage.setScene(scene);
-		primaryStage.setWidth(GameConstants.WIDTH);
-		primaryStage.setHeight(GameConstants.HEIGHT);
-	    primaryStage.show();
         
+		primaryStage.setTitle("Geometry Dash");
+		primaryStage.setScene(scene);
+	    primaryStage.show();
     }
 
     public static void main(String[] args) {
