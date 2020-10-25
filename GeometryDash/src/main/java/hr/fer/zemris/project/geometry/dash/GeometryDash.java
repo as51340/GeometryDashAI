869ca515973cef462141f1f8a64777fd7889c108
Andi Škrgat <as51340@fer.hr>
2020-10-25 20:35:44 +0100

    GeometryDash -main refactoring for starting app

--- GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/GeometryDash.java
+++ GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/GeometryDash.java
@@ -23,16 +23,55 @@ public class GeometryDash extends Application {
 
 	private GameEngine gameEngine = new GameEngine(100, "Geometry Dash", GameConstants.WIDTH, GameConstants.HEIGHT);
 	
+	
+	private void loadGameMenu(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(
+	    		getClass().getResource(GameConstants.pathToVisualization + "BackgroundScene.fxml")
+	    	);
+	    	
+	        // Set a default "standard" or "100%" resolution
+	        double origW = GameConstants.WIDTH;
+	        double origH = GameConstants.HEIGHT;
+
+	        // Place the Parent in a StackPane, which will keep it centered
+	        StackPane rootPane = new StackPane();
+	        rootPane.getChildren().add(root);
+
+	        // Create the scene initially at the "100%" size
+	        Scene scene = new Scene(rootPane, origW, origH);
+	        
+	        // Bind the scene's width and height to the scaling parameters on the group
+	        root.scaleXProperty().bind(scene.widthProperty().divide(origW));
+	        root.scaleYProperty().bind(scene.heightProperty().divide(origH));
+
+	        // Preserve aspect ratio when resizing
+	        double ratio = (double) GameConstants.HEIGHT / GameConstants.WIDTH;
+	        primaryStage.minHeightProperty().bind(primaryStage.widthProperty().multiply(ratio));
+	        primaryStage.maxHeightProperty().bind(primaryStage.widthProperty().multiply(ratio));
+	        
+			primaryStage.setTitle("Geometry Dash");
+			primaryStage.setScene(scene);
+		    primaryStage.show();
+	}
+	
+	private void loadMain(Stage primaryStage) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(GameConstants.pathToVisualization + "GameScene.fxml"));
+    	Parent root1 = fxmlLoader.load();
+    	GameSceneController controller = fxmlLoader.<GameSceneController>getController();
+    	controller.setGameEngine(gameEngine);
+    	Scene scene = new Scene(root1);
+    	gameEngine.createStageFromData(primaryStage);
+    	gameEngine.start();
+    	primaryStage.setTitle("Geometry Dash");
+		primaryStage.setScene(scene);
+	    primaryStage.show();
+	}
+	
     @Override
     public void start(Stage primaryStage) throws IOException {
-//    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(GameConstants.pathToVisualization + "GameScene.fxml"));
-//    	Parent root1 = fxmlLoader.load();
-//    	GameSceneController controller = fxmlLoader.<GameSceneController>getController();
-//    	controller.setGameEngine(gameEngine);
-//    	Scene scene = new Scene(root1);
-//    	gameEngine.createStageFromData(primaryStage);
-//    	gameEngine.start();
-        
+
+//        loadGameMenu(primaryStage);
+    	loadMain(primaryStage);
 //    	tester
 //		Media media = new Media(getClass().getResource(GameConstants.pathToSongs + "BlahBlahBlah.mp3").toExternalForm());
 //		MediaPlayer mediaPlayer = new MediaPlayer(media);
@@ -41,37 +80,10 @@ public class GeometryDash extends Application {
 //		Utils.createMediaPlayer("BlahBlahBlah.mp3").play();
 //		System.out.println("Printaj odmah poslije");
     	
-    	Parent root = FXMLLoader.load(
-    		getClass().getResource(GameConstants.pathToVisualization + "BackgroundScene.fxml")
-    	);
-    	
-        // Set a default "standard" or "100%" resolution
-        double origW = GameConstants.WIDTH;
-        double origH = GameConstants.HEIGHT;
-
-        // Place the Parent in a StackPane, which will keep it centered
-        StackPane rootPane = new StackPane();
-        rootPane.getChildren().add(root);
-
-        // Create the scene initially at the "100%" size
-        Scene scene = new Scene(rootPane, origW, origH);
-        
-        // Bind the scene's width and height to the scaling parameters on the group
-        root.scaleXProperty().bind(scene.widthProperty().divide(origW));
-        root.scaleYProperty().bind(scene.heightProperty().divide(origH));
-
-        // Preserve aspect ratio when resizing
-        double ratio = (double) GameConstants.HEIGHT / GameConstants.WIDTH;
-        primaryStage.minHeightProperty().bind(primaryStage.widthProperty().multiply(ratio));
-        primaryStage.maxHeightProperty().bind(primaryStage.widthProperty().multiply(ratio));
-        
-		primaryStage.setTitle("Geometry Dash");
-		primaryStage.setScene(scene);
-	    primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
