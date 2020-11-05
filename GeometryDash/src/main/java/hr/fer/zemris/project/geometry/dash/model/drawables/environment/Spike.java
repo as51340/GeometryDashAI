fcdc0f273f780b1905869f7d4da828be407a8662
Matej Skrabic <matej.skrabic@fer.hr>
2020-11-05 14:05:50 +0100

    Collisons with spikes done (?)

--- GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/model/drawables/environment/Spike.java
+++ GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/model/drawables/environment/Spike.java
@@ -7,7 +7,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 public class Spike extends Obstacle {
-	//note: position here is lowe left corner!
+	//note: position here is lower left corner!
 	public Spike(Vector2D position) {
 		setCurrentPosition(position);
 		this.setWidth(GameConstants.iconHeight); //stranica a
@@ -31,16 +31,15 @@ public class Spike extends Obstacle {
 	@Override
 	public boolean contains(Vector2D p) {
 		double yp = p.getY();
-		double sx = this.getCurrentPosition().getX();
-		double sy = this.getCurrentPosition().getY();
 		double a = this.getHeight();
+		double sx = this.getCurrentPosition().getX();
+		double sy = this.getCurrentPosition().getY()+a;
 
-		double x1 = sx + sx/(-2.0*a)*(yp-sy);
-		double x2 =(yp - sy)*(sx-2.0*a)/(-2.0*a)+sx+a;
+		double x1 = sx + sy/2 - (yp)/2;
+		double x2 = sx + a + (yp-sy)/2;
 
 		return (yp>=sy-a && yp<=sy)&&
-                ((x1>=p.getX() && x1<=p.getX()+a)
-                || (x2>=p.getX() && x2<=p.getX()+a));
+                ((x1>=p.getX() && x1<=p.getX()+a) || (x2>=p.getX() && x2<=p.getX()+a));
 	}
 
 	@Override
