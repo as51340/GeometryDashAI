7367902a2071b9978718b165e36b0a2e69927c57
kBlaic <kb51942@fer.hr>
2020-12-07 01:15:18 +0100

    added previous and next buttons to ChooseLevel

--- GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/visualization/ChooseLevelController.java
+++ GeometryDash/src/main/java/hr/fer/zemris/project/geometry/dash/visualization/ChooseLevelController.java
@@ -1,5 +1,36 @@
 package hr.fer.zemris.project.geometry.dash.visualization;
 
-public class ChooseLevelController extends MainOptionsController {
+import java.util.Set;
 
+import hr.fer.zemris.project.geometry.dash.model.level.Level;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Line;
+
+public class ChooseLevelController extends MainOptionsController {
+	
+//	private Set<Level> levels = gameEngine.getLevelManager().getAllLevels();
+
+	@FXML
+	private Line line;
+	
+	@FXML
+	private ImageView backgroundImage;
+	
+	@FXML
+	private ImageView previousLevel;
+	
+	@FXML
+	private ImageView nextLevel;
+
+	@FXML
+	private void nextButtonClicked() {
+		
+	}
+	
+	@FXML
+	private void previousButtonClicked() {
+		
+	}
+	
 }
